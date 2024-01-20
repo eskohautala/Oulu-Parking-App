6c48c9068f00982c47e0266572e16eec5db950f4
Esko Hautala <esko.hautala@student.oulu.fi>
2024-01-20 13:43:00 +0200

    Oulu Parking App Done

--- app/page.js
+++ app/page.js
@@ -1,34 +1,35 @@
-import image from './Images/Oulu_logo.jpg'
+import image from "./Images/Oulu_logo.jpg";
 export default async function Home() {
   let carParks = await fetchData();
 
   return (
     <main>
-    
-      <div class="bg"> 
-        <div class="title-bg-blur">
-            <h1 className='text-4xl text-center justify-content p-24 text-white font-bold '> Oulun Kaupungin Pysäköintitilastot Kohteittain 
-          
-            </h1>
+      <div className="bg">
+        <div className="title-container p-16">
+          <h2 className="title text-4xl text-white rounded-xl">
+            Oulun Kaupungin Pysäköintitilastot Kohteittain
+          </h2>
         </div>
 
-    <ul className='grid grid-cols-6 gap-4 flex min-h-screen flex-col items-center justify-content'> 
-        {carParks.map((carPark) =>
-        <li key ={carPark.carParkId}>
-          <span className='block p-2 border border-gray-300 rounded'>
-          {carPark.name} <br></br> Paikkoja Vapaana: {carPark.spacesAvailable ? carPark.spacesAvailable: 'Ei saatavilla'}
-          </span>
-        </li>
-        )}
-    </ul>
-    </div>
+        <ul className="grid grid-cols-6 gap-4 min-h-screen flex-col items-center text-white p-4">
+          {carParks.map((carPark) => (
+            <li key={carPark.carParkId}>
+              <span className="block p-4 rounded-lg grid-blur text-center grid-cards">
+                {carPark.name} <br></br> Paikkoja Vapaana:{" "}
+                {carPark.spacesAvailable
+                  ? carPark.spacesAvailable
+                  : "Ei saatavilla"}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </main>
-  )
- }
+  );
+}
 // Funktio fetch kutsun tekemiseen, palauttaa json tiedoston carParks muuttujaan
-  async function fetchData() {
-
-    const query = `
+async function fetchData() {
+  const query = `
     query GetAllCarParks {
       carParks {
         carParkId
@@ -39,20 +40,16 @@ export default async function Home() {
     }
     `;
 
-    const response = await fetch("https://api.oulunliikenne.fi/proxy/graphql", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-      }, 
-      body: JSON.stringify({
-        query
-      }) 
-
-    }); 
-    const data = await response.json();
-    return data.data.carParks;
-} 
-
-
-
+  const response = await fetch("https://api.oulunliikenne.fi/proxy/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      query,
+    }),
+  });
+  const data = await response.json();
+  return data.data.carParks;
+}
